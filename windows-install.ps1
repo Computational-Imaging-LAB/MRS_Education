edbc30ea07822405345099724d149150736d1379
Abdullah Baş <34542006+abdullahbas@users.noreply.github.com>
2021-11-30 03:07:55 +0300

    Add files via upload

---
+++ windows-install.ps1
@@ -0,0 +1,11 @@
+
+
+
+wget "https://bounedutr-my.sharepoint.com/:u:/g/personal/abdullah_bas_boun_edu_tr/EbJFMbwftAlMlGzVIEOLxxEBaw_QRVg359QGtRqsafl6Lw?e=gZa0g8&download=1" -OutFile requested.zip
+Expand-Archive requested.zip -DestinationPath requested_image
+wget https://download.virtualbox.org/virtualbox/6.1.30/VirtualBox-6.1.30-148432-Win.exe -OutFile virtualbox.exe
+.\deneme.exe
+
+
+
+
