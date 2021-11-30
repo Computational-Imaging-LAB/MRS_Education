e0c4dd8bd073333135a4ac0153588dc65c7db763
Abdullah Baş <34542006+abdullahbas@users.noreply.github.com>
2021-11-30 10:20:50 +0300

    Update windows-install.ps1

--- windows-install.ps1
+++ windows-install.ps1
@@ -1,7 +1,7 @@
 
 
 
-wget "https://bounedutr-my.sharepoint.com/:u:/g/personal/abdullah_bas_boun_edu_tr/EbJFMbwftAlMlGzVIEOLxxEBaw_QRVg359QGtRqsafl6Lw?e=gZa0g8&download=1" -OutFile requested.zip
+wget "https://bounedutr-my.sharepoint.com/:f:/g/personal/abdullah_bas_boun_edu_tr/EpY4XDQJ0BlOowvjEaHkVKkBoo73uXOMMz7jMYaCI7SO4g?e=283K338&download=1" -OutFile requested.zip
 Expand-Archive requested.zip -DestinationPath requested_image
 wget https://download.virtualbox.org/virtualbox/6.1.30/VirtualBox-6.1.30-148432-Win.exe -OutFile virtualbox.exe
 .\deneme.exe
