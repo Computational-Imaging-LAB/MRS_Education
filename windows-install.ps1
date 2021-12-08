cdf3ab55ec7411abea2afd3f1774c85aaa0a1bbd
Abdullah Baş <34542006+abdullahbas@users.noreply.github.com>
2021-12-09 01:03:08 +0300

    Update windows-install.ps1

--- windows-install.ps1
+++ windows-install.ps1
@@ -1,7 +1,7 @@
 
 
 
-wget "https://bounedutr-my.sharepoint.com/:u:/g/personal/abdullah_bas_boun_edu_tr/Edkpj8CNFthFsHEjeckH54wBjjbkUr-hCWSG9RXnqQVw3Q?e=pFPK97&download=1" -OutFile requested.zip
+wget "https://bounedutr-my.sharepoint.com/:u:/g/personal/abdullah_bas_boun_edu_tr/Edkpj8CNFthFsHEjeckH54wBjjbkUr-hCWSG9RXnqQVw3Q?e=523wHz&download=1" -OutFile requested.zip
 Expand-Archive requested.zip -DestinationPath requested_image
 wget https://download.virtualbox.org/virtualbox/6.1.30/VirtualBox-6.1.30-148432-Win.exe -OutFile virtualbox.exe
 .\virtualbox.exe
