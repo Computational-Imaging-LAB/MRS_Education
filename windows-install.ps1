ac6fd06cbb28278a6ab327ff991d6be1aa086ddf
Abdullah Baş <34542006+abdullahbas@users.noreply.github.com>
2021-12-24 14:49:53 +0300

    Update windows-install.ps1

--- windows-install.ps1
+++ windows-install.ps1
@@ -1,7 +1,15 @@
 
 
+echo "This process may take 3-5 hours (~12gb) depends on your internet connection. Wait until finish it will not output any progressbar to make things faster. There will be a prompt after finishing the download"
 
-wget "https://bounedutr-my.sharepoint.com/:u:/g/personal/abdullah_bas_boun_edu_tr/EXNeyaVOjqNFjEmA7NIWWGsBAu3lOBcGNzzmJojlURUZWQ?e=3XWHTA&download=1" -OutFile requested.zip
+
+$ProgressPreference = 'SilentlyContinue'
+wget  "https://bounedutr-my.sharepoint.com/:u:/g/personal/abdullah_bas_boun_edu_tr/EXNeyaVOjqNFjEmA7NIWWGsBAu3lOBcGNzzmJojlURUZWQ?e=3XWHTA&download=1" -OutFile requested.zip 
+$ProgressPreference = 'Continue'
+
+echo "Download has finished."
+
+
 Expand-Archive requested.zip -DestinationPath requested_image
 wget https://download.virtualbox.org/virtualbox/6.1.30/VirtualBox-6.1.30-148432-Win.exe -OutFile virtualbox.exe
 .\virtualbox.exe
