51d00ce5bcb4965907a81c80fac0b0bfdd831d7c
Abdullah Baş <34542006+abdullahbas@users.noreply.github.com>
2022-01-04 15:10:51 +0300

    Update req-install.sh

--- req-install.sh
+++ req-install.sh
@@ -21,14 +21,4 @@ echo "***********************************************************************"
 echo "Installation is finished. Now close and open the terminal again."
 echo "***********************************************************************"
 
-echo "deb http://cz.archive.ubuntu.com/ubuntu bionic main universe" |  sudo tee -a /etc/apt/sources.list
-sudo apt-get update && sudo apt-get upgrade
-sudo apt install make gcc libgtk-3-dev libwebkitgtk-dev libwebkitgtk-3.0-dev libgstreamer-gl1.0-0 freeglut3 freeglut3-dev python-gst-1.0 python3-gst-1.0 libglib2.0-dev ubuntu-restricted-extras libgstreamer-plugins-base1.0-dev
 
-
-conda create --name mrs_course python=3.7
-conda activate mrs_course
-conda install numpy scipy matplotlib wxpython configobj packaging
-conda install -c conda-forge lmfit pydicom pypubsub
-pip install pygamma vespa-suite
-python -m vespa.check_dependencies
