7b88084784fe6abb54a90e03bc74bb37c43e3eef
Abdullah Baş <34542006+abdullahbas@users.noreply.github.com>
2022-03-08 15:40:51 +0300

    Update vespa-install.sh

--- vespa-install.sh
+++ vespa-install.sh
@@ -7,7 +7,6 @@ echo "deb http://cz.archive.ubuntu.com/ubuntu bionic main universe" |  sudo tee 
 sudo apt install make gcc libgtk-3-dev libwebkitgtk-dev libwebkitgtk-3.0-dev libgstreamer-gl1.0-0 freeglut3 /
                       freeglut3-dev python-gst-1.0 python3-gst-1.0 libglib2.0-dev ubuntu-restricted-extras /
                       libgstreamer-plugins-base1.0-dev
-conda create --name mrs_course python=3.7
 conda activate mrs_course
 conda install numpy scipy matplotlib wxpython configobj packaging
 conda install -c conda-forge lmfit pydicom pypubsub
